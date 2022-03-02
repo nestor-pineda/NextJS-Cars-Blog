a0430e6d22088d4c0d0829fafe61beccc2075d3f
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-03-02 09:13:59 +0100

    added space and access token

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
 }
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+
+  return <div className="recipe-list">Recipe List</div>;
+}
+
+// then we return the res object and with props we pass the items to our recepies component.
